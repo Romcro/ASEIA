3f9a6a7edb6b2c7a62bd49d4c05709640ab635c0
Koneko Design Studio <licence@konekodesignstudio.fr>
2024-03-23 22:45:40 +0100

    Create header.php

---
+++ header.php
@@ -0,0 +1,11 @@
+<header>	
+	<img class="logoheader" src="images/logo/logoaseia.svg" alt="logo ASEIA">
+
+	<h2 id="h2">
+
+		 Assistanat et Secrétariat pour Entreprises, Indépendants et Associations 
+
+	</h2>
+
+	<balise id="entete"></balise> 
+</header>	
