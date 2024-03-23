2029dc596bc2444d46614a6c0f8ada6bbf13bb9b
Koneko Design Studio <licence@konekodesignstudio.fr>
2024-03-23 22:45:17 +0100

    Create conditions.php

---
+++ conditions.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="Cache-control" content="public/private/no-cache/no-store">
+
+	<meta name="description"
+    content="" />
+
+ 	<meta name="keywords"
+    content="" />
+	
+	<title>ACCUEIL ASEIA, Assistanat et Secrétariat pour Entreprises, Indépendants et Associations. Conditions génerales de ventes</title>
+
+
+   	<link rel="stylesheet" href="styles/normalize.css">
+	<link rel="stylesheet" href="ASEIA.css">
+	
+	<link rel="icon" type="image/png" href="favicon-96x96.png" />
+
+	
+
+</head>
+<body>
+
+<?php include('header.php'); ?>
+
+<?php include('nav.php'); ?>
+	
+	<section id="texte">
+	
+	<p>
+	
+		<h3>CONDITIONS GÉNÉRALES DE VENTE</h3><br><br>
+
+La microentreprise ASEIA, représentée par Madame Amandine SCHREPEL, désignée ci-après par « le vendeur » agit en qualité d’intervenant auprès de tout particulier, professionnel, collectivité, société, association ou personne signataire, désignés ci-après par « le client », l’ayant sollicitée. Les présentes conditions générales de vente régissent de façon exclusive toutes les relations commerciales entre le vendeur et le client. Toutes autres conditions n’engagent le vendeur qu’après confirmation écrite de sa part.
+		<br><br>
+		
+Le seul fait de passer une commande ou d’accepter une offre du vendeur comporte l’acceptation sans réserve des présentes conditions générales. Les présentes conditions générales peuvent être modifiées à tout moment et sans préavis par le vendeur, les modifications étant alors applicables aux commandes postérieures. <br><br>
+		
+		
+
+<h3>Article 1 - Objet</h3><hr><br>
+Clause générale
+Les conditions générales de vente décrites ci-après détaillent les droits et obligations du vendeur et de son client dans le cadre de la vente des prestations de services suivantes <br>
+– Code APE 8219Z : « photocopie, préparation de documents et autres activités spécialisées de soutien de bureau »
+– Toute activité d’assistance pour la gestion administrative, commerciale, bureautique ou financière ; ainsi que toute activité de traduction, transcription audio, conseil, photographie. <br>
+Le fait de passer commande implique l’adhésion entière et sans réserve du client à ces conditions générales de prestations de services. Le client renonce ainsi à toute application de ses éventuelles conditions générales d’achat. Les prestations prévues à l’article 1 sont soumises aux présentes conditions générales qui prévalent sur toute condition d'achat, sauf dérogation formelle et expresse de notre part. <br>
+Lieux d’exécution
+Les prestations prévues à l’article 1 sont réalisables dans les locaux d’ASEIA (à distance) ou dans les locaux du client. Les conditions de réalisation seront précisées dans le devis. Le vendeur s’engage à donner ses meilleurs soins, conformément aux règles de l’art. La présente obligation n’est de convention expresse que pure obligation de moyen. <br><br>
+
+<h3>Article 2 - Confidentialité</h3><hr> <br>
+Chaque partie s’engage à conserver strictement confidentiels les données, informations ou documents concernant l’autre partie de quelque nature qu’ils soient (économiques, techniques, commerciaux) auxquels elle pourrait avoir connaissance à l’occasion du présent contrat. Les documents remis ou envoyés par nous-mêmes demeurent notre propriété ; ils ne peuvent donc être communiqués à des tiers sous quelque motif que ce soit par l'acheteur, sauf accord express de notre part. <br>
+
+En application de la loi n°78-17 du 6 janvier 1978, il est rappelé que le Client dispose donc d’un droit d’accès, de rectification ou d’opposition s’agissant des informations le concernant, dans les délais prévus par la loi et la réglementation en vigueur. <br>
+
+En adhérant à ces conditions générales de vente, le client consent à ce que nous collections et utilisions les données le concernant telles que : raison sociale, nom commercial, adresse mail, numéro de téléphone, numéro SIRET, et toute information le concernant nécessaire à la réalisation du présent contrat et au bon fonctionnement de la relation commerciale, notamment à l’établissement du devis et à la facturation. <br><br>
+
+<h3>Article 3 - Disponibilité</h3><hr><br>
+Le vendeur effectue ses prestations selon sa disponibilité et celle du Client. <br>
+En cas de déplacement sur site, il convient au client de prévenir dans un délai minimum de 72 heures à l’avance. <br><br>
+
+<h3>Article 4 - Obligation de collaboration</h3><hr><br>
+Le client doit mettre à la disposition du vendeur, dès sa commande, l’ensemble des informations utilisables et nécessaires à la bonne réalisation de la prestation. Tout document fourni par le client ne pouvant être traité (document manuscrit illisible, …) fera l’objet de l’annulation du contrat. <br><br>
+
+<h3>Article 5 - Devis et commandes</h3><hr><br>
+Lorsqu'un devis est établi par nous, il constitue les conditions particulières venant modifier ou compléter les présentes conditions générales. Pour chaque demande d’intervention ponctuelle, le vendeur établit un devis sans aucun engagement du client, contenant le détail de la prestation à effectuer. Cette proposition de prix est établie en deux exemplaires : un pour le vendeur, l’autre pour le client. Ce devis vaut bon de commande quand le client le retourne dûment accepté, daté, signé, en portant la mention « Bon pour Accord » au vendeur. Si le devis est transmis au client par mail, il devient bon de commande quand le client envoie au vendeur un mail d’accord concernant l’acceptation du devis. <br><br>
+
+<h3>Article 6 – Tarifs</h3><hr><br>
+Les tarifs pratiqués par le vendeur ne comprennent ni les fournitures, ni les coûts d’impression, de sauvegarde, ni les frais postaux, ni les frais de déplacement. Les tarifs sont donnés à titre indicatif et ne remplacent en aucun cas un devis en bonne et due forme, étudié selon vos besoins spécifiques. <br>
+Les prix des différentes prestations de services sont exprimés en euros (€), et calculés hors taxes. Ils ne sont pas majorés du taux de TVA, le statut d’autoentrepreneur impliquant une franchise de TVA, selon l’article 293 B du CGI. Le vendeur s’engage à facturer les prix indiqués lors de l’enregistrement de la commande. Aucun escompte ne sera accordé pour paiement anticipé. <br><br>
+
+<h3>Article 7 – Conditions de paiement</h3><hr><br>
+Modalités : <br>
+Sauf mention contraire sur le devis préalablement établi, le règlement des prestations se fait au comptant à réception de la facture y compris celles intermédiaires, sauf conditions particulières stipulées expressément au contrat, proposition commerciale ou convention. <br>
+Le règlement des factures, y compris celles intermédiaires, s’effectue : <br>
+– soit par chèque bancaire ou postal à l’ordre de : Madame Amandine SCHREPEL <br>
+– soit par virement bancaire <br>
+
+Chaque annulation de commande doit se faire auprès du vendeur par écrit ou par messagerie électronique à l’adresse contact@aseia.fr dans un délai minimum de 8 jours ouvrés précédant la réalisation prévue du service. Toute commande annulée totalement ou partiellement par le client sans le consentement du vendeur, ou en dehors du délai minimum prévu, sera facturée quelle qu'en soit la cause. Dans tous les cas, si un acompte a été versé, il ne sera pas restitué. <br>
+Erreurs de facturation : <br>
+Toute réclamation concernant des erreurs de facturation doit être adressée au vendeur dans un délai maximal de 8 jours suivant la réception de la facture. Passé ce délai, toute contestation sera forclose. <br>
+Sanction du retard de paiement : <br>
+En cas de défaut partiel ou total de paiement des prestations réalisées, le client devra verser au vendeur une pénalité de retard égale à 10 % du montant de la facture par mois de retard. <br>
+Cette pénalité est calculée sur la somme restant due, et court à compter de la date d’échéance sans qu’aucune mise en demeure préalable ne soit nécessaire. <br>
+Le montant de l’indemnité forfaitaire pour frais de recouvrement prévu est fixé à 40 euros ; elle sera due de plein droit et sans formalité par le professionnel en situation de retard. Le défaut de paiement d'un seul effet ou d'une seule facture à son échéance autorisera en outre l’entreprise à suspendre les prestations ultérieures. <br>
+Si une commande s'exécute en plusieurs prestations, les factures afférentes à chacune des prestations sont payables au fur et à mesure de leur émission sans attendre l'exécution entière de la commande. Le défaut de paiement d'une seule d'entre elles ou le défaut d'acceptation de la traite y afférant nous autorise, sans mise en demeure préalable, à tenir ladite commande pour résiliée et entraîne de droit la rupture de toute relation contractuelle. <br><br>
+
+<h3>Article 8 - Livraisons</h3><hr><br>
+Modalités de livraison : 
+La livraison des services sera effectuée selon les modalités et conditions définies sur le devis. Le vendeur s'autorise à ne pas livrer la prestation si le client n'est pas à jour de toutes ses obligations.
+Délais : <br>
+Les délais de livraison sont donnés à titre purement indicatif, et sans garantie du vendeur. Les retards de livraisons ne peuvent, en aucun cas, donner lieu à l'annulation de la commande, au refus des services commandés ou à l'octroi de pénalités ou de dommages et intérêts. <br>
+Suspension et retard de livraison : <br>
+Le vendeur se réserve le droit, sans que le client ne puisse prétendre à une quelconque indemnité, de suspendre, retarder ou modifier l'exécution des commandes dans les cas fortuits ou de force majeure. Est un cas de force majeure tout événement indépendant de la volonté du vendeur et faisant obstacle au fonctionnement normal de son activité. Constituent notamment des cas de force majeure les grèves totales ou partielles de l’un de ses fournisseurs, sous-traitants ou transporteurs ainsi que l’interruption des transports, de la fourniture d’énergie, de matières premières ou de pièces détachées. En outre, la force majeure s’entend de tout événement extérieur, imprévisible et irrésistible au sens de l’article 1148 du Code civil. <br>
+Contrôle de la qualité des services : <br>
+Toute réclamation au sujet d’un service réalisé par le vendeur pour le compte du client doit être adressée dans les 8 jours suivant la réalisation effective du service, par lettre recommandée avec demande d'avis de réception. Passé ce délai, les services concernés ne pourront donner lieu au versement de dommages et intérêts au client. <br><br>
+
+<h3>Article 9 - Clause résolutoire</h3><hr><br>
+En cas d'inexécution de ses obligations par une partie, le contrat sera résolu de plein droit au profit de l'autre partie sans préjudice des dommages et intérêts qui pourraient être réclamés à la partie défaillante. La résolution prendra effet 15 jours après l'envoi d'une mise en demeure restée infructueuse. <br><br>
+
+<h3>Article 10 – Litige</h3><hr><br>
+Les présentes conditions générales de vente sont soumises aux droits français. Le vendeur met en œuvre tous les moyens à sa disposition pour prendre soin et préserver les fichiers informatiques et autres documents ou informations qui lui sont confiés par le Client pour la réalisation de sa prestation. L’entreprise ne peut être tenue pour responsable des dommages de toute nature, tant matériels qu'immatériels ou corporels, qui pourraient résulter d'un mauvais fonctionnement ou de la mauvaise utilisation des services réalisés, ou pour une erreur engendrée par un manque d’informations ou des informations erronées. Il en est de même pour les éventuelles modifications ultérieures des services, réalisées par le compte de tiers, et l’entreprise ne sera pas tenue pour responsable des incompatibilités informatiques dues aux versions de logiciels, ou aux systèmes d’exploitation, utilisés par le client. Toutefois, compte tenu des risques de dommages ou de détériorations encourus par ce type de support, il appartient au Client de s’en prémunir par tous moyens à sa convenance. La responsabilité du vendeur ne peut donc pas être engagée à ce titre. La responsabilité du vendeur sera, en tout état de cause, limitée au montant de la commande et ne saurait être mise en cause pour de simples erreurs ou omissions qui auraient pu subsister malgré toutes les précautions prises dans la présentation des services. Le Client convient que le vendeur n’encourt aucune responsabilité à raison de toute perte de bénéfices, de trouble commercial, de demande que le Client subirait, de demandes ou de réclamations formulées contre le Client et émanant d’un tiers, quel qu’il soit. En cas de difficultés dans l'application du présent contrat, le client a la possibilité, avant toute action en justice, de rechercher une solution amiable notamment avec l'aide : <br>
+-d'une association professionnelle de la branche, <br>
+-d'une association de consommateurs ou de tout autre conseil de son choix. <br>
+Il est rappelé que la recherche de la solution amiable n'interrompt pas le " bref délai" de la garantie légale, ni la durée de la garantie contractuelle. Il est rappelé qu'en règle générale et sous réserve de l'appréciation des Tribunaux, le respect des dispositions du présent contrat relatives à la garantie contractuelle suppose que le client honore ses engagements financiers envers le vendeur. Les réclamations ou contestations seront toujours reçues avec bienveillance attentive, la bonne foi étant toujours présumée chez celui qui prend la peine d'exposer ses situations. En cas de litige, le client s'adressera par priorité à l'entreprise pour obtenir une solution amiable. <br>
+À défaut, la chambre commerciale du tribunal de grande instance de Metz (57) est seule compétente, quels que soient le lieu de livraison ou de réalisation et le mode de paiement acceptés. <br><br>
+	
+	</p>
+</section>
+	
+<?php include('footer.php'); ?>
+
+<?php include('conceptiongraphique.php'); ?>
+
+<?php include('script.php'); ?>
+
+<body>
+</body>
+</html>
