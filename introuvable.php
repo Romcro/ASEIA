0a54822430094af01becc83aaeb62e6799c53bd6
Koneko Design Studio <licence@konekodesignstudio.fr>
2024-03-23 22:45:48 +0100

    Create introuvable.php

---
+++ introuvable.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-129788818-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-129788818-1');
+</script>
+	
+	
+	
+	<meta charset="UTF-8">
+<meta http-equiv="Cache-Control" content="max-age=600" />
+
+<meta name="description" content=" PAGE 404 ASEIA 
+"> 
+
+ 	<meta name="keywords"
+    content="" />
+	
+	<title>ACCUEIL ASEIA, Assistanat et Secrétariat pour Entreprises, Indépendants et Associations, PAGE 404 </title>
+
+
+
+ 	<link rel="stylesheet" href="styles/normalize.css">
+	<link rel="stylesheet" href="introuvable.css">
+	
+	<link rel="icon" type="image/png" href="favicon-96x96.png" />
+
+<body>
+	
+	
+	  <section class="center">
+    <article>
+      <h1 class="header">404</h1>
+      <p class="error">ERROR</p>
+    </article>
+    <article>
+      <img src="http://i62.tinypic.com/vovg1x.png" alt="Funny Face">
+
+
+
+      <ul>
+        <li><a href="index.php">Home</a></li>
+
+      </ul>
+	
+
+</body>
+</html>
