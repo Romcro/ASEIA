3cf8b4782893f964fa1ee35d7be9f042bdb8e7d7
Koneko Design Studio <licence@konekodesignstudio.fr>
2024-03-23 22:46:07 +0100

    Create relecture.php

---
+++ relecture.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-129788818-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-129788818-1');
+</script>
+	
+	
+	
+	
+	<meta charset="UTF-8">
+	<meta http-equiv="Cache-Control" content="max-age=600" />
+
+<meta name="description" content=" « RELECTURE ET CORRECTION »
+En tant que professionnel, il est essentiel de soigner l’image que renvoie votre structure. Dans la société actuelle, où les fautes se glissent jusque dans les campagnes de publicité à échelle nationale, décrédibilisant même les plus grandes entreprises, nous vous proposons de vous démarquer par des textes irréprochables sur tous vos supports.
+
+
+Faites confiance à notre secrétaire certifiée en orthographe et grammaire au niveau Expert (Certification Voltaire) pour que votre image reflète la qualité de votre entreprise jusque dans les détails.
+
+Nos prestations de relecture et correction peuvent concerner :
+
+ -Tous supports commerciaux : site internet, applications, publicités, etc.
+
+ -Mails et courriers.
+
+-Articles internet et presse.
+
+ -Manuscrits (roman, nouvelles, etc ).
+
+ -Thèses, mémoires.
+"
+
+> 
+	
+	<title>CATEGORIE RELECTURE ASEIA, Assistanat et Secrétariat pour Entreprises, Indépendants et Associations, page de la catégorie relecture</title>
+
+
+   	<link rel="stylesheet" href="styles/normalize.css">
+	<link rel="stylesheet" href="ASEIA.css">
+
+	<link rel="icon" type="image/png" href="favicon-96x96.png" />
+
+	
+
+</head>
+
+
+<body>
+
+<?php include('header.php'); ?>
+
+<?php include('nav.php'); ?>
+
+	
+
+<section id="pleinelargeur">
+
+<img class="pleinelargeur" src="images/photos/relecture.jpg" alt="photo de la categorie relecture">
+
+</section>
+
+<section id="lignehaut">
+	
+</section>
+
+
+<section id="texte">
+
+<h1>« RELECTURE ET CORRECTION ».</h1><br><br><br>
+
+<p>En tant que professionnel, il est essentiel de soigner l’image que renvoie votre structure. Dans la société actuelle, où les fautes se glissent jusque dans les campagnes de publicité à échelle nationale, décrédibilisant même les plus grandes entreprises, <span style="font-weight : bold">nous vous proposons de vous démarquer par des textes irréprochables sur tous vos supports.</span><br><br>
+
+
+Faites confiance à notre secrétaire <span style="font-weight : bold">certifiée en orthographe et grammaire au niveau Expert (Certification Voltaire)</span> pour que votre image reflète la qualité de votre entreprise jusque dans les détails.<br><br>
+
+<span style="font-weight : bold">Nos prestations de relecture et correction peuvent concerner :</span><br><br>
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; -Tous supports commerciaux : site internet, applications, publicités, etc.<br><br>
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; -Mails et courriers.<br><br>
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; -Articles internet et presse.<br><br>
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; -Manuscrits (roman, nouvelles, etc ).<br><br>
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; -Thèses, mémoires.<br><br>
+
+Liste non-exhaustive. Pour toute autre requête, <a href="formulairedecontact.php">contactez-nous.</a></p>
+
+</section>
+
+
+
+
+
+<?php include('footer.php'); ?>
+
+<?php include('conceptiongraphique.php'); ?>
+
+<?php include('script.php'); ?>
+	
+</body>
+</html>
