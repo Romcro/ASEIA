137236896e52a45df007eef77b7d6350b9155644
Koneko Design Studio <licence@konekodesignstudio.fr>
2024-03-23 22:46:05 +0100

    Create nosvaleurs.php

---
+++ nosvaleurs.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-129788818-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-129788818-1');
+</script>
+	
+	
+	
+	<meta charset="UTF-8">
+	<meta http-equiv="Cache-Control" content="max-age=600" />
+
+<meta name="description" content="« NOS VALEURS ».
+
+Chez ASEIA, nous sommes particulièrement sensibles aux problématiques écologiques actuelles. C’est donc tout naturellement que nous avons pris le parti d’utiliser au maximum des outils dématérialisés afin d’économiser le plus possible les ressources matérielles comme le papier, tout en étant attentifs à notre impact.  
+
+Nous avons également fait le choix d’opérer à distance afin de réduire drastiquement nos déplacements, et donc notre empreinte carbone. Nous rencontrons toutefois nos clients dès que cela s’avère indispensable. 
+
+"> 	
+	
+	
+	<title>NOS VALEURS ASEIA, Assistanat et Secrétariat pour Entreprises, Indépendants et Associations, page nos valeurs</title>
+
+
+    <link rel="stylesheet" href="styles/normalize.css">
+	<link rel="stylesheet" href="ASEIA.css">
+	
+	<link rel="icon" type="image/png" href="favicon-96x96.png" />
+	
+	
+
+</head>
+
+
+<body>
+
+<?php include('header.php'); ?>
+
+<?php include('nav.php'); ?>
+
+	
+
+<section id="pleinelargeur">
+
+<img class="pleinelargeur" src="images/photos/nosvaleurs.jpg" alt="photo nos valeurs">
+
+</section>
+
+<section id="lignehaut">
+	
+</section>
+
+
+<section id="texte">
+
+
+
+<h1>« NOS VALEURS ».</h1><br><br><br>
+
+<p>Chez ASEIA, nous sommes particulièrement sensibles aux problématiques écologiques actuelles. C’est donc tout naturellement que nous avons pris le parti d’utiliser au maximum des outils dématérialisés afin d’économiser le plus possible les ressources matérielles comme le papier, tout en étant attentifs à notre impact.  <br><br><br>
+
+Nous avons également fait le choix d’opérer à distance afin de réduire drastiquement nos déplacements, et donc notre empreinte carbone. Nous rencontrons toutefois nos clients dès que cela s’avère indispensable. </p>
+
+
+</section>
+
+
+
+
+
+<?php include('footer.php'); ?>
+
+<?php include('conceptiongraphique.php'); ?>
+
+<?php include('script.php'); ?>
+
+	
+</body>
+</html>
