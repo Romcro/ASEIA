81c98657086619c451b0dec679546649b9eefc1f
Koneko Design Studio <licence@konekodesignstudio.fr>
2024-03-23 22:44:51 +0100

    Create assistanceadministrative.php

---
+++ assistanceadministrative.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-129788818-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-129788818-1');
+</script>
+	
+	
+	
+<meta charset="UTF-8">
+<meta http-equiv="Cache-Control" content="max-age=600" />
+
+<meta name="description" content="« ASSISTANCE ADMINISTRATIVE »
+
+>Pour la gestion globale de votre structure, nous vous proposons :
+
+Aide rédactionnelle </span> : saisie et mise en page de tous documents
+
+Gestion de vos mails :
+
+ - Réception : filtrer les mails inutiles et vous permettre de ne lire que l’essentiel
+
+ Envoi : relances, demandes d’informations, communications diverses, etc.
+
+Aide aux démarches administratives avec les diverses instances publiques et privées.
+
+Conseil en organisation afin d’optimiser votre temps."> 
+
+
+	<title>CATEGORIE ASSISTANCE ADMINISTRATIVE ASEIA, Assistanat et Secrétariat pour Entreprises, Indépendants et Associations, catégorie assistance admnistrative</title>
+
+
+    <link rel="stylesheet" href="styles/normalize.css">
+	<link rel="stylesheet" href="ASEIA.css">
+	
+	<link rel="icon" type="image/png" href="favicon-96x96.png" />
+	
+	
+
+</head>
+
+
+<body>
+
+<?php include('header.php'); ?>
+
+<?php include('nav.php'); ?>
+
+	
+
+<section id="pleinelargeur">
+
+<img class="pleinelargeur" src="images/photos/assistanceadministrative.jpg" alt="photo assistance administrative">
+
+</section>
+
+<section id="lignehaut">
+	
+</section>
+
+
+<section id="texte">
+
+
+
+<h1>« ASSISTANCE ADMINISTRATIVE ».</h1><br><br><br>
+
+<p>Pour la gestion globale de votre structure, nous vous proposons :<br><br><br>
+
+&bull; <span style="font-weight : bold">Aide rédactionnelle </span> : saisie et mise en page de tous documents<br><br>
+
+&bull; <span style="font-weight : bold">Gestion de vos mails :</span><br><br><br>
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Réception : filtrer les mails inutiles et vous permettre de ne lire que l’essentiel.<br><br>
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Envoi : relances, demandes d’informations, communications diverses, etc.<br><br><br>
+
+&bull; <span style="font-weight : bold">Aide aux démarches administratives </span> avec les diverses instances publiques et privées.<br><br><br>
+
+&bull; <span style="font-weight : bold">Conseil en organisation afin d’optimiser votre temps.</span><br><br><br>
+
+
+
+Liste non-exhaustive. Pour toute autre requête, <a href="formulairedecontact.php">contactez-nous.</a></p>
+
+
+
+
+
+</section>
+
+
+
+
+
+
+<?php include('footer.php'); ?>
+
+<?php include('conceptiongraphique.php'); ?>
+
+<?php include('script.php'); ?>
+
+
+</body>
+</html>
