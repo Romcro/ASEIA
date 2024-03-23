55faf0b7d077cd9e2a6fb4b0fd4fc1da71bbd1cf
Koneko Design Studio <licence@konekodesignstudio.fr>
2024-03-23 22:45:30 +0100

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,38 @@
+
+
+<footer>
+
+	<section id="footer">
+
+
+					<article>
+
+
+
+
+
+					</article>
+
+
+
+					<article id>
+
+					<ul id="lienfooter">
+							<a href="formulairedecontact.php">CONTACT
+						</a>
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+							<a href="conditions.php"target="_blank" >C.G.V</a>
+							
+							
+					</ul>
+					</article>
+
+	</section>
+
+	
+
+
+
+	
+</footer>
+
