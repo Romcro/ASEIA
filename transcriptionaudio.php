b0a1e6ce13984db1d1de56fa498457029d4b73fc
Koneko Design Studio <licence@konekodesignstudio.fr>
2024-03-23 22:46:18 +0100

    Create transcriptionaudio.php

---
+++ transcriptionaudio.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-129788818-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-129788818-1');
+</script>
+	
+	
+	
+	
+	<meta charset="UTF-8">
+	<meta http-equiv="Cache-Control" content="max-age=600" />
+
+<meta name="description" content=" « TRANSCRIPTION AUDIO ».
+
+Nous nous chargeons de saisir à l’écrit et de mettre en forme tous vos enregistrements audio dans la plus grande confidentialité :
+
+
+Comptes-rendus, rapports.
+
+ -Réunions, assemblées générales, conseils d’administration.
+
+ -Conférences, colloques, congrès.
+
+ -Interviews, entretiens.
+
+ -Thèses, mémoires.
+
+Vous déterminez à l’avance le type de retranscription désirée (intégrale ou épurée, style direct ou indirect), ainsi que vos consignes en termes de mise en page,nous nous occupons du reste.
+
+
+"> 	
+	
+	<title>CATEGORIE TRANSCRIPTION AUDIO ASEIA, Assistanat et Secrétariat pour Entreprises, Indépendants et Associations, page de la catégorie transcription audio
+	</title>
+
+
+    <link rel="stylesheet" href="styles/normalize.css">
+	<link rel="stylesheet" href="ASEIA.css">
+	
+	<link rel="icon" type="image/png" href="favicon-96x96.png" />
+
+
+
+</head>
+
+
+<body>
+
+<?php include('header.php'); ?>
+
+<?php include('nav.php'); ?>
+
+	
+
+<section id="pleinelargeur">
+
+<img class="pleinelargeur" src="images/photos/transcriptionaudio.jpg" alt="photo de la categorie transcrption audio">
+
+</section>
+
+<section id="lignehaut">
+	
+</section>
+
+
+<section id="texte">
+
+<h1>« TRANSCRIPTION AUDIO ».</h1><br><br><br>
+
+<p>Nous nous chargeons de saisir à l’écrit et de mettre en forme <span style="font-weight : bold">tous vos enregistrements audio dans la plus grande confidentialité :</span><br><br><br>
+
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; -Comptes-rendus, rapports.<br><br>
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; -Réunions, assemblées générales, conseils d’administration.<br><br>
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; -Conférences, colloques, congrès.<br><br>
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; -Interviews, entretiens.<br><br>
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; -Thèses, mémoires.<br><br><br>
+
+<p>Vous déterminez à l’avance le type de retranscription désirée (intégrale ou épurée, style direct ou indirect), ainsi que vos consignes en termes de mise en page, <span style="font-weight : bold">nous nous occupons du reste.</span></p><br><br>
+
+Liste non-exhaustive. Pour toute autre requête, <a href="formulairedecontact.php">contactez-nous.</a></p>
+
+</section>
+
+
+
+
+
+<?php include('footer.php'); ?>
+
+<?php include('conceptiongraphique.php'); ?>
+
+<?php include('script.php'); ?>
+	
+</body>
+</html>
