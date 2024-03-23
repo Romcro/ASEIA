b9e7eb7516c697085ce37c5edf34e6dbdd8136f0
Koneko Design Studio <licence@konekodesignstudio.fr>
2024-03-23 22:45:57 +0100

    Create nav.php

---
+++ nav.php
@@ -0,0 +1,36 @@
+
+	<nav>
+			<div id="menuresp-ct">
+				<div id="menuresp">
+					<div class="menucross">
+						<div class="menucross-ct">
+							<span></span>
+							<span></span>
+							<span></span>
+						</div>
+					</div>
+					<div class="menuct">
+						<ul>
+							<li><a href="index.php" class="home">Accueil</a></li>
+							
+							<li class="sousmenu"><a class="smenu">prestations<i class="material-icons">&#xE313;</i></a>
+								<ul>
+									<li><a href="assistanceadministrative.php">assistance administrative</a></li>
+									<li><a href="relecture.php">relecture et correction</a></li>
+									<li><a href="assistancelinguistiqueanglais.php">assistance linguistique anglais</a></li>
+									<li><a href="transcriptionaudio.php">transcription audio</a></li>
+
+
+								</ul>
+							</li>
+							
+							<li><a href="nosvaleurs.php">nos valeurs</a></li>
+							<li><a href="formulairedevis.php">demandez un devis</a></li>
+							<li><a href="formulairedecontact.php">contact</a></li>
+							<li><a href="#">Mentions légales</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="pb-ombre"></div>
+			</div>
+</nav>		
