7f22b38f349668a9e14d6228f868f93a8dcb4280
Koneko Design Studio <licence@konekodesignstudio.fr>
2024-03-23 22:45:43 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-129788818-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-129788818-1');
+</script>
+	
+	
+	
+	<meta charset="UTF-8">
+<meta http-equiv="Cache-Control" content="max-age=600" />
+
+<meta name="description" content="Bienvenue chez ASEIA, secrétariat indépendant qui assiste à distance les professionnels dans la gestion de leur activité au quotidien. C’est d’ailleurs l’essence même de notre nom :
+
+					« Assistanat et Secrétariat pour Entreprises, Indépendants et Associations ».
+
+		Vous êtes de plus en plus chargés par le poids de la gestion administrative, avec toujours plus de formalités à gérer ?
+
+		Cela entraîne :
+
+		 - Un manque de temps pour développer votre activité ?
+
+		 - Un stress supplémentaire au quotidien ?
+
+		 - Une vie professionnelle qui vient empiéter sur votre vie privée ? 
+
+		Nous vous offrons les solutions à ces problèmes en vous allégeant de cette charge administrative.
+
+		ASEIA vous propose ainsi des prestations de qualité, selon vos besoins, pour des missions ponctuelles ou récurrentes.<br><br>
+		Vous n’avez aucune contrainte : pas d’engagement, pas d’embauche, pas de charge salariale, pas de déplacement.
+"> 
+
+ 	<meta name="keywords"
+    content="" />
+	
+	<title>ACCUEIL ASEIA, Assistanat et Secrétariat pour Entreprises, Indépendants et Associations, page d'accueil</title>
+
+
+
+    <link rel="stylesheet" href="styles/normalize.css">
+	<link rel="stylesheet" href="ASEIA.css">
+	
+	<link rel="icon" type="image/png" href="favicon-96x96.png" />
+
+	
+
+</head>
+<body>
+
+<?php include('header.php'); ?>
+
+<?php include('nav.php'); ?>
+
+<section id="pleinelargeur">
+<img class="pleinelargeur" src="images/photos/pleinecran-aseia.jpg" alt="Images plein ecran ASEIA,Assistanat et Secrétariat pour Entreprises, Indépendants et Associations">
+</section>
+
+<section id=lignehaut>
+	
+</section>
+
+
+<section id="texte">
+
+		<p>Bienvenue chez ASEIA, secrétariat indépendant qui assiste à distance les professionnels dans la gestion de leur activité au quotidien. C’est d’ailleurs l’essence même de notre nom :<br><br><br>
+
+			<h1>
+					« Assistanat et Secrétariat pour Entreprises, Indépendants et Associations ».
+
+			</h1>
+
+		<br><br><br>
+
+		Vous êtes de plus en plus chargés par le poids de la gestion administrative, avec toujours plus de formalités à gérer ?<br><br>
+
+		Cela entraîne :</p><br>
+
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Un manque de temps pour développer votre activité ?<br>
+
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Un stress supplémentaire au quotidien ?<br>
+
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Une vie professionnelle qui vient empiéter sur votre vie privée ? <br><br><br>
+
+		Nous vous offrons les solutions à ces problèmes en vous allégeant de cette charge administrative.<br><br>
+
+		ASEIA vous propose ainsi des prestations de qualité, selon vos besoins, pour des missions ponctuelles ou récurrentes.<br><br>
+		Vous n’avez aucune contrainte : <span style="font-weight : bold">pas d’engagement, pas d’embauche, pas de charge salariale, pas de déplacement.</span><br><br>
+
+		Naviguez à travers nos rubriques pour découvrir le détail de nos prestations ou <a href="formulairedecontact.php">contactez-nous.</a>.</p>
+
+</section>
+
+
+
+<!--POLYGONE---->
+
+			</section>
+
+
+			<section id=sectionpoly>
+
+					<a href="assistanceadministrative.php">
+						<div class="container">
+  								<img src="images/photos/assistanceadministrative.jpg" alt="Avatar" id="polygon" >
+ 				 			<div class="middle">
+    							<div class="text">Assistance Administrative</div>
+ 							</div>
+						</div>
+					</a>
+
+
+
+					<a href="assistancelinguistiqueanglais.php">
+						<div class="container">
+  								<img src="images/photos/traduction.jpg" alt="Avatar" id="polygon" >
+ 				 			<div class="middle">
+    							<div class="text">Assistance Linguistique Anglais</div>
+ 							</div>
+						</div>
+					</a>
+
+			</section>		
+
+			<section id="sectionpolybas">
+
+					<a href="relecture.php">
+						<div class="container">
+  								<img src="images/photos/relecture.jpg" alt="Avatar" id="polygon" >
+ 				 			<div class="middle">
+    							<div class="text">Relecture et Correction</div>
+ 							</div>
+						</div>
+					</a>
+
+					<a href="transcriptionaudio.php">
+						<div class="container">
+  								<img src="images/photos/transcriptionaudio.jpg" alt="Avatar" id="polygon" >
+ 				 			<div class="middle">
+    							<div class="text">Transcription Audio</div>
+ 							</div>
+						</div>
+					</a>	
+
+			</section>		
+	
+			</section>	
+
+
+<?php include('footer.php'); ?>
+
+<?php include('conceptiongraphique.php'); ?>
+
+<?php include('script.php'); ?>
+
+
+
+	
+</body>
+</html>
