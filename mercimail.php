3450697ac34b4a772825c2f94d0794fdab4e41fb
Koneko Design Studio <licence@konekodesignstudio.fr>
2024-03-23 22:45:52 +0100

    Create mercimail.php

---
+++ mercimail.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="Cache-control" content="public/private/no-cache/no-store">
+
+	<meta name="description"
+    content="" />
+
+ 	<meta name="keywords"
+    content="" />
+
+    <meta http-equiv="refresh" content="5; URL=http:index.php">
+	
+	<title>CATEGORIE ASSISTANCE ADMINISTRATIVE ASEIA, Assistanat et Secrétariat pour Entreprises, Indépendants et Associations</title>
+
+
+    <link rel="stylesheet" href="styles/normalize.css">
+	<link rel="stylesheet" href="ASEIA.css">
+	
+	<link rel="icon" type="image/png" href="favicon-96x96.png" />
+	
+	
+
+</head>
+
+
+<body>
+
+
+<section id="texte">
+
+<p>
+	<span style="font-weight : bold">Chaque message est lu avec la plus grande attention,</span> mais nous ne sommes pas à l’abri d’un problème informatique. Si vous pensez que nous n’avons pas reçu votre message, n’hésitez pas à nous contacter à nouveau.<br><br> Après tout, <span style="font-weight : bold">nous sommes là pour prendre soin de vous.</span>
+</p>
+
+
+
+
+
+
+</section>
+
+
+
+
+
+
+<?php include('footer.php'); ?>
+
+<?php include('conceptiongraphique.php'); ?>
+
+<?php include('script.php'); ?>
+
+
+</body>
+</html>
