a12023e3f1835478664049b91efe4cc5504607f8
Koneko Design Studio <licence@konekodesignstudio.fr>
2024-03-23 22:45:13 +0100

    Create assistancelinguistiqueanglais.php

---
+++ assistancelinguistiqueanglais.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="fr"><head>
+	
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-129788818-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-129788818-1');
+</script>
+	
+	
+	
+	<meta charset="UTF-8">
+	<meta http-equiv="Cache-control" content="public/private/no-cache/no-store">
+
+	<meta http-equiv="Cache-Control" content="max-age=600" />
+
+<meta name="description" content="« ASSISTANCE LINGUISTIQUE EN ANGLAIS ».
+
+Vous souhaitez développer votre activité à l’international  mais vous ne vous sentez pas capable de prospecter en anglais ?Vous avez déjà refusé des propositions venant de l’étranger car vous ne pensiez pas pouvoir gérer la communication commerciale en anglais ?
+
+Nous vous proposons de nous occuper de toute la communication de votre entreprise avec vos prospects et clients étrangers en anglais, LA langue internationale par excellence.Faites confiance à notre traductrice certifiée par le TOEIC (Test of English for International Communication).
+
+Nos prestations d’assistance linguistique.
+
+Traduction vers l’anglais de tous vos supports commerciaux (site internet, application mobile, etc.)
+
+Traduction vers l’anglais des devis, bons de commande et factures.
+
+Gestion de la communication avec vos clients internationaux.
+"> 	
+	<title>CATEGORIE ASSISTANCE LINGUISTIQUE ANGLAIS ASEIA, Assistanat et Secrétariat pour Entreprises, Indépendants et Associations, catégorie assistance linguistique en anglais</title>
+
+    <link rel="stylesheet" href="styles/normalize.css">
+	<link rel="stylesheet" href="ASEIA.css">
+	
+	<link rel="icon" type="image/png" href="favicon-96x96.png" />
+	
+	
+
+</head>
+
+
+<body>
+
+<?php include('header.php'); ?>
+
+<?php include('nav.php'); ?>
+
+	
+
+<section id="pleinelargeur">
+
+<img class="pleinelargeur" src="images/photos/traduction.jpg" alt="photo categorie assistance linguistique anglais">
+
+</section>
+
+<section id="lignehaut">
+	
+</section>
+
+
+<section id="texte">
+
+<h1>« ASSISTANCE LINGUISTIQUE EN ANGLAIS ».</h1><br><br>
+
+<p>Vous souhaitez développer votre activité à l’international <span style="font-weight : bold">mais vous ne vous sentez pas capable de prospecter en anglais ?</span> Vous avez déjà refusé des propositions venant de l’étranger car <span style="font-weight : bold">vous ne pensiez pas pouvoir gérer la communication commerciale en anglais ?</span><br><br>
+
+Nous vous proposons de nous occuper de toute la communication de votre entreprise avec vos prospects et clients étrangers en anglais, LA langue internationale par excellence. <span style="font-weight : bold">Faites confiance à notre traductrice certifiée par le TOEIC (Test of English for International Communication).</span><br><br>
+
+<span style="font-weight : bold">Nos prestations d’assistance linguistique.</span> <br><br>
+
+&bull;Traduction vers l’anglais de tous vos supports commerciaux (site internet, application mobile, etc.)<br><br>
+
+&bull;Traduction vers l’anglais des devis, bons de commande et factures.<br><br>
+
+&bull; Gestion de la communication avec vos clients internationaux.<br><br>
+
+Liste non-exhaustive. Pour toute autre requête, <a href="formulairedecontact.php">contactez-nous.</a></p>
+
+
+
+</section>
+
+
+
+
+
+
+<?php include('footer.php'); ?>
+
+<?php include('conceptiongraphique.php'); ?>
+
+<?php include('script.php'); ?>
+
+	
+</body>
+</html>
